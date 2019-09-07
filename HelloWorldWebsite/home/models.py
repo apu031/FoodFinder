461a9519cf80072ca8ae35646951c5d9f2f286cb
Mac <mac@Macs-MacBook.local>
2019-09-07 17:41:20 -0600

    create drop down menus for cities and food categories

--- HelloWorldWebsite/home/models.py
+++ HelloWorldWebsite/home/models.py
@@ -4,6 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+CITY_CHOICES = (
+    ('toronto', 'toronto'),
+    ('edmonton', 'edmonton'),
+    ('new york', 'new york'),
+    ('montreal', 'montreal'),
+    ('ottawa', 'ottawa'),
+)
 
-class Counter(models.Model):
-    count = models.IntegerField(blank=False, default=0)
+FOOD_CATEGORIES = (
+    ('french', 'french'),
+    ('indian', 'indian'),
+)
+
+
+class DropDownModel(models.Model):
+    cities = models.CharField(max_length=6, choices=CITY_CHOICES, default='green')
+    food_categories = models.CharField(max_length=6, choices=FOOD_CATEGORIES, default='orange')
+
--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -5,19 +5,14 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django.views import generic
-from .models import Counter
+from django.shortcuts import render
 
 # Create your views here.
-class Home(generic.DetailView):
-    model = Counter
+from django.views.generic import CreateView
+from .models import DropDownModel
+from .forms import DropDownModelForm
+
+class Home(CreateView):
+    model = DropDownModel
     template_name = "home/index.html"
-
-    def get(self, request, *args, **kwargs):
-        context = {'our_counter' : Counter.objects.get(pk=1)}
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        counter_object = Counter.objects.get(pk=1)
-        counter_object.count += 1
-        counter_object.save()
-        return redirect('homepage')
+    form_class = DropDownModelForm
