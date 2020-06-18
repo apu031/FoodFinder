8574c794f39ebc270f183363351c5cb28f8f836a
apu031 <apu.islam@ualberta.ca>
2020-06-18 00:29:53 -0600

    wsgi updated for project name change

--- FoodFinder/FoodFinder/wsgi.py
+++ FoodFinder/FoodFinder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HelloWorldWebsite project.
+WSGI config for FoodFinder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HelloWorldWebsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FoodFinder.settings")
 
 application = get_wsgi_application()
