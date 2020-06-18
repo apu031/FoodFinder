809d32033c1a7c4c892548b0170b58e6b3ad2b98
apu031 <apu.islam@ualberta.ca>
2020-06-18 00:30:50 -0600

    models are updated

--- FoodFinder/food/models.py
+++ FoodFinder/food/models.py
@@ -6,12 +6,6 @@ from django.db import models
 # Create your models here.
 CITY_CHOICES = (
     ('Toronto', 'Toronto'),
-    ('New york', 'New york'),
-    ('Montreal', 'Montreal'),
-    ('Ottawa', 'Ottawa'),
-    ('Phoenix', 'Phoenix'),
-    ('Calgary', 'Calgary'),
-    ('Saint Joseph', 'Saint Joseph'),
 )
 
 
@@ -22,7 +16,7 @@ class DropDownModel(models.Model):
 class ParsedData(models.Model):
     business_id = models.TextField(primary_key=True, blank=False)
     name = models.TextField(blank=True, null=True)
-    city = models.TextField(blank=True, choices=CITY_CHOICES, null=True)
+    city = models.TextField(blank=True, null=True)
     stars = models.TextField(blank=True, null=True)
     review_count = models.TextField(blank=True, null=True)
     categories = models.TextField(blank=True, null=True)
